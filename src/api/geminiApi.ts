0a2b06fedceb5ee5c773af9b361e1a9ecde72403
Boris Trochez <boristrochez@seti.com.co>
2025-02-14 10:38:43 -0500

    Hotfix: Fixed [Gemini returning internal error]

--- src/api/geminiApi.ts
+++ src/api/geminiApi.ts
@@ -10,14 +10,14 @@ export const fetchGeminiResponse = async ({
   favoriteIngredient2,
 }: RecipePromptConditions) => {
   const prompt = `Create a healthy, delicious recipe for Boris (age: ${age}, weight: ${weight}kg, height: ${height}cms) for ${foodTime}. Include 2 favorite ingredients: ${favoriteIngredient1} and ${favoriteIngredient2}. Provide concise instructions in 5 steps or fewer`;
-  const promptOptimized = `short recipe, ingredients: ${favoriteIngredient1}, ${favoriteIngredient2}`;
+  const promptOptimized = `really short recipe for: ${favoriteIngredient1}, ${favoriteIngredient2}`;
   try {
     const response = await axios.post(
       import.meta.env.VITE_API_LAMBDA_URL,
       { prompt: promptOptimized },
       {
         headers: { "Content-Type": "application/json" },
-      },
+      }
     );
     return response.data;
   } catch (error) {
